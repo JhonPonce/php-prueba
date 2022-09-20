2b0427f3d68af140e2fb65c26545775214bb0fbd
JhonPonce <jhon_tsh2@hotmail.com>
2022-09-20 10:44:46 -0500

    Creacion de ejemplo

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+// METODOLOGIA DE PDO();
+$db = new PDO('mysql:host=localhost; dbname=financiera','Sise','Sise2021');
+$sql = "SELECT * FROM cliente";
+$listado = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado - PDO</title>
+</head>
+<body>
+    <?php 
+        foreach($listado as $cliente):
+    ?>
+        <h3><?php echo $cliente['razon_social']?></h3>
+        <ul></ul>
+    <?php
+        endforeach;
+    ?>
+</body>
+</html>
